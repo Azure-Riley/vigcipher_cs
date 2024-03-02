af8b6cd7550c0eeb4a94bb46f308d51f3f6e927b
Azure-Riley <azureriley12@gmail.com>
2024-03-02 20:05:06 +0100

    Added encryption option in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 using System;
+using cipher;
 
 class Program {
     static public void Main(String[] args){
-        Console.WriteLine("hello");
+        Console.WriteLine("Hello, do you want to encrypt[0] or decrypt[1]?");
+        Console.Write("Choice: ");
+        int option = Convert.ToInt32(Console.ReadLine());
+
+         switch (option){
+            case 0:
+                normalToVig.encryptMsg();
+                break;   
+
+            default:
+                Console.WriteLine("Under development");
+                break;   
+         }
     }
 }
+
